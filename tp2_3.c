83a498225502a068289489bae90c90d0436198a5
Agustin <agustinxpa@gmail.com>
2022-03-31 19:01:22 -0300

    terminado del punto 3 tp2_3.c

---
+++ tp2_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+#define N 5
+#define M 7
+    int i, j;
+    int mt[N][M];
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+           /* mt[i][j] = 1 + rand() % 100;*/
+            *(*(mt+i)+j)=1 + rand() % 100;
+            printf("%i ", mt[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
