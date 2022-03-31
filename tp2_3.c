50c1f99c6852c99fc6dd693bdd779ff05b37d01e
Agustin <agustinxpa@gmail.com>
2022-03-31 19:48:23 -0300

    correcion punto 3

--- tp2_3.c
+++ tp2_3.c
@@ -6,14 +6,42 @@ int main(int argc, char const *argv[])
 #define M 7
     int i, j;
     int mt[N][M];
-
+    int *pmt;
+    pmt=mt[0];
+    int vt[N][M];
     for (i = 0; i < N; i++)
     {
         for (j = 0; j < M; j++)
         {
            /* mt[i][j] = 1 + rand() % 100;*/
             *(*(mt+i)+j)=1 + rand() % 100;
-            printf("%i ", mt[i][j]);
+        printf("%i ", mt[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+           /* mt[i][j] = 1 + rand() % 100;*/
+        //*(mt+1)=1 + rand() % 100;
+        *(pmt+(i*M+j))=1 + rand()%100;
+
+      //  printf("%i ", mt[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+           /* mt[i][j] = 1 + rand() % 100;*/
+        //*(mt+1)=1 + rand() % 100;
+        //*(pmt+(i*M+j))=1 + rand()%100;
+
+        printf("%i ", mt[i][j]);
         }
         printf("\n");
     }
